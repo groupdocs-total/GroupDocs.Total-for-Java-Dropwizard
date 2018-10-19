918c029cec3941b925652f7618be46c1a5b72380
Pavel Egorov <egorovpasha@gmail.com>
2018-10-19 11:38:31 +0100

    fixed path to assets and added signature dep.

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,5 @@ var gulp = require('gulp')
 gulp.task('build', function() {
   return gulp
     .src('./node_modules/@groupdocs.examples.jquery/**')
-    .pipe(gulp.dest('./src/main/resources/static/assets/'))
+    .pipe(gulp.dest('./src/main/resources/assets/'))
 })

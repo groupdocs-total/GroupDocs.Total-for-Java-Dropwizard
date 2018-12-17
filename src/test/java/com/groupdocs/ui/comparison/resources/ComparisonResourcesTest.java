671a5b78c1a10538077a0b51491a4c386a632962
Elena Kolesnikova <koleslena@yandex.ru>
2018-12-17 15:34:09 +0600

    added test for load file tree

--- src/test/java/com/groupdocs/ui/comparison/resources/ComparisonResourcesTest.java
+++ src/test/java/com/groupdocs/ui/comparison/resources/ComparisonResourcesTest.java
@@ -2,35 +2,60 @@ package com.groupdocs.ui.comparison.resources;
 
 import com.groupdocs.ui.common.MainService;
 import com.groupdocs.ui.common.config.GlobalConfiguration;
+import com.groupdocs.ui.common.entity.web.request.FileTreeRequest;
 import io.dropwizard.client.JerseyClientBuilder;
-import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.glassfish.jersey.client.ClientProperties;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 import java.io.File;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ComparisonResourcesTest {
     @ClassRule
     public static final DropwizardAppRule<GlobalConfiguration> RULE =
             new DropwizardAppRule<>(MainService.class, System.getProperty("user.dir") + File.separator +"configuration.yml");
-    @Test
-    public void getView() {
-        Client client = new JerseyClientBuilder(RULE.getEnvironment()).build("test client");
+
+    private static Client client;
+
+    @BeforeClass
+    public static void setupClass() {
+        client = new JerseyClientBuilder(RULE.getEnvironment()).build("test client");
         client.property(ClientProperties.CONNECT_TIMEOUT, 5000);
         client.property(ClientProperties.READ_TIMEOUT,    5000);
+    }
+
+    @AfterClass
+    public static void clearClass() {
+        client.close();
+    }
+
+    @Test
+    public void getView() {
         Response response = client.target(
                 String.format("http://localhost:%d/comparison", RULE.getLocalPort()))
                 .request()
                 .get();
         assertThat(response.getStatus()).isEqualTo(200);
-        client.close();
     }
 
+    @Test
     public void loadFileTree() {
+        FileTreeRequest fileTreeRequest = new FileTreeRequest();
+        fileTreeRequest.setPath("");
+
+        Response response = client.target(
+                String.format("http://localhost:%d/comparison/loadFileTree", RULE.getLocalPort()))
+                .request()
+                .post(Entity.json(fileTreeRequest));
+        assertThat(response.getStatus()).isEqualTo(200);
     }
 
     public void downloadDocument() {

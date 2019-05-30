9f29d71a3c6466ee22d735acea353754c98f5311
Elena Kolesnikova <koleslena@yandex.ru>
2019-05-30 22:09:11 +0600

    removed config

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,3 @@ gulp.task('copy', function() {
       .src('./node_modules/@groupdocs.examples.angular/**')
       .pipe(gulp.dest('./src/main/resources/assets/angular/'))
 });
-
-gulp.task('config', function() {
-  return gulp
-      .src('./node_modules/@groupdocs.examples.angular/viewer/dist/assets/config/**')
-      .pipe(gulp.dest('./src/main/resources/assets/config/'))
-});

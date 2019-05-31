6bbbd06c6e9e5274ea23582f0b202a8750cde603
koleslena <koleslena@yandex.ru>
2019-05-31 12:37:21 +0600

    Added support for Angular front-end(#53)

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,10 @@ gulp.task('build', function() {
   return gulp
     .src('./node_modules/@groupdocs.examples.jquery/**')
     .pipe(gulp.dest('./src/main/resources/assets/'))
-})
+});
+
+gulp.task('copy', function() {
+  return gulp
+      .src('./node_modules/@groupdocs.examples.angular/**')
+      .pipe(gulp.dest('./src/main/resources/assets/angular/'))
+});

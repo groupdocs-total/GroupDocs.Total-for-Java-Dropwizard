a76b349626cf94d35aea70a732b3974d082d826a
Pavel Egorov <egorovpasha@gmail.com>
2019-07-24 11:53:01 +0400

    fixed npm install issue on windows

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -3,5 +3,5 @@ var gulp = require('gulp')
 gulp.task('copy', function() {
   return gulp
     .src('./node_modules/@groupdocs.examples.jquery/**')
-    .pipe(gulp.dest('./src/main/resources/assets/'))
+    .pipe(gulp.dest('./../src/main/resources/assets/'))
 });
